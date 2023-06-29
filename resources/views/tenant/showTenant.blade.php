7d3cbbca8456b89745425dad4cac308a5ef711da
Eive <g.kristhabayu1.0@gmail.com>
2023-06-29 11:23:20 +0700

    edit button simpan and batal containner

---
+++ resources/views/tenant/showTenant.blade.php
@@ -0,0 +1,84 @@
+@extends('layouts.admin-dashboard')
+@push('styles')
+    @vite(['resources/css/add-style.css'])
+@endpush
+@section('title', 'Edit Tenant')
+@section('content')
+    <div class="container_content">
+        <div class="title_content">
+            <div class="title">
+                <label for="id_tenant">{{$tenant->id_tenant}}</label>
+            </div>
+        </div>
+        <div class="container">
+            <div class="titleBar">
+                <div class="subtitle text-center">Data Tenant</div>
+            </div>
+            <div class="container-fluid mx-5">
+                <form action="{{ route('tenant.update', $tenant)}}" method="post">
+                    @csrf
+                    @method('PATCH')
+                    <input type="hidden" name="id_tenant" value="{{ $tenant->id_tenant }}">
+                    <table cellspacing="10px" class="form_tenant">
+                        <tr>
+                            <td style="padding-left: 10px;">
+                                <label>Nama Pemilik</label>
+                            </td>
+                            <td><label>:</label></td>
+                            <td>
+                                <div class="input-field">
+                                    <label
+                                        type="text"
+                                        class="input"
+                                        placeholder={{$tenant->nama_tenant}}
+                                    name="nama_tenant"
+                                        value="{{ old('nama_tenant', $tenant->nama_tenant) }}"
+                                    />
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="padding-left: 10px;">
+                                <label>Kategori</label>
+                            </td>
+                            <td><label>:</label></td>
+                            <td>
+                                <div class="input-field">
+                                    <label
+                                        type="text"
+                                        class="input"
+                                        placeholder={{$tenant->nama_tenant}}
+                                    name="nama_tenant"
+                                        value="{{ old('nama_tenant', $tenant->nama_tenant) }}"
+                                    />
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="padding-left: 10px;">
+                                <label>Nomor Telepon</label>
+                            </td>
+                            <td><label>:</label></td>
+                            <td>
+                                <div class="input-field">
+                                    <label
+                                        type="text"
+                                        class="input"
+                                        placeholder={{$tenant->no_telp}}
+                                    name="no_telp"
+                                        value="{{ old('no_telp', $tenant->no_telp) }}"
+                                    />
+                                </div>
+                            </td>
+                        </tr>
+                    </table>
+                    <div class="button-container">
+                        <button type="submit" class="buttonSimpan">Simpan</button>
+                        <a href="{{ route('tenant.index') }}">
+                        <button type="button" class="buttonBatal">Batal</button>
+                    </a>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
